fa8d43985dfcbfba67e161544dbf4b5f47946a6d
Christine Chang <cyc058@ucsd.edu>
2017-02-13 17:18:16 -0800

    Edited exercisePage.js and created json file

--- routes/exercisePage.js
+++ routes/exercisePage.js
@@ -1,7 +1,12 @@
-exports.viewExPage = function(req,res) {
-	var name = req.params.name;
-	console.log("The exercise name is: " + name);
+
+	var data = require('../exerciseList.json');
+
+	exports.viewExPage = function(req,res){
+		console.log(data);
+		res.render('exercisePage');
+	};
+	
+	/*console.log("The exercise name is: " + name);
 	res.render('exercisePage', {
 		'exName' : name
-	});
-};
+	});*/
