112e1b1d16822ab3097af269627dc3a30dc9e431
Christine Chang <cyc058@ucsd.edu>
2017-02-16 20:29:33 -0800

    link parts

--- routes/exercises.js
+++ routes/exercises.js
@@ -1,13 +1,33 @@
 
-var data = require('../exerciseList.json');
+var dat = require('../exerciseList.json');
 
+function getExercises(obj){
+		var result =[];
+		var i;
+		for(i=0;i<dat.exercises.length; i++){
+			if (dat.exercises[i].bodyPart == obj){
+				result.push(dat.exercises[i].id);
+			}
+		}
 
+		return result;
+}
 
 exports.viewExercises = function(req,res){
   // console.log(data);
-  var pts = { name: "head"}
-  data.part.push(pts)
-  console.log(data)
-  res.render('exercises', data);
+  //var pts = { name: "head"}
+  var name = req.params.name;
+  var list = getExercises(name);
+
+  //data.part.push(pts)
+  console.log(list);
+  res.render('exercises', {
+  		exId : list,
+  		body : name
+  });
+  res.render('exercises', dat);
+
+  console.log(dat.exercises[0].bodyPart);
+  //res.render('exercises', data);
 };
 
--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,8 @@
+
+var data = require('../exerciseList.json');
+
 exports.viewHome = function(req,res) {
 	var name = req.params.name;
 	console.log("I hope this works");
-	res.render('home', {
-		'homeName' : name
-	});
+	res.render('home',data);
 };
