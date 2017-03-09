a3cc683402eb987814386a2bbe1f5ea155497131
Alvin Mo <almo@ucsd.edu>
2017-03-08 19:31:07 -0800

    analytics ready

--- public/js/habitrabbit.js
+++ public/js/habitrabbit.js
@@ -1,23 +1,16 @@
 
 	$(document).ready(function() {
-		//initializePage();
+		initializePage();
 		$("#back-button").click(function() {
 		window.history.back();
 	});
 
 	});
 
-/*
 function initializePage() {
-	//adds exercise to favorites EVENTUALLY
-	$('.fav').click(addFav);
+	$('.viewedExercise').click(function () {
+		ga("send", "event", "view", "click");
+	});
 
+	
 }
-
-function addFav(e) {
-	e.preventDefault();
-	//from lab6 get div ID,
-	var exerciseID = $(this).closest('.exercise').attr('id');
-	console.log("User clicked on exercise " + exerciseID);
-
-*/
